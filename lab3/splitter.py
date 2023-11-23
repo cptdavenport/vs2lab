ce5d61df31f407dab50ce73056bc000271a1c751
Stefan Maier <git@univ-exp.com>
2023-11-23 09:29:12 +0100

    abgabe version

--- lab3/splitter.py
+++ lab3/splitter.py
@@ -3,7 +3,7 @@ from rich.console import Console
 from zmq4.log import setup_logging
 from zmq4.splitter import Splitter
 
-setup_logging(log_level="ERROR")
+setup_logging(log_level="INFO")
 s = Splitter(limit_sentences=100)
 console = Console()
 console.input("Press RETURN to start? :rocket: ")
